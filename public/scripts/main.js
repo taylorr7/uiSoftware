4c0510e28976ada71feed2577c1213d2f9e37222
Steven-Roberts <steven94@vt.edu>
2016-10-27 13:32:27 -0400

    Start update from ES5 to ES6

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -7,13 +7,7 @@
 * The text in the search bar is set to "Search" by
 * default and is cleared when the user clicks it.
 */
-$(document).ready(function(){
-	$('input[name="mainSearch"]').val("Search");
-
-	$('input[name="mainSearch"]').click(function() {
-		$('input[name="mainSearch"]').val("");
-	});
-
+$(document).ready(() => {
 	/*
 	* Function to allow the user to subscribe to a course.
 	*/
@@ -53,7 +47,7 @@ $(document).on('click', '.collapse li a', function() {
 * Function to bring up the help menu.
 * Right now just displays an alert to the user.
 */
-help = function() {
+const help = () => {
 	alert("Help");
 }
 
@@ -61,7 +55,7 @@ help = function() {
 * Function to send the user to a given page.
 * Used to turn buttons into a navigation tool.
 */
-sendToPage = function(destination) {
+const sendToPage = (destination) => {
 	destinationString = destination;
 	location.href = destinationString;
 }
@@ -70,42 +64,42 @@ sendToPage = function(destination) {
 * Function used to validate the input sent
 * by the user when registering for the site.
 */
-validateForm = function() {
-	uname = document.forms["register"]["user"].value;
-	pass = document.forms["register"]["pass"].value;
-	vpass = document.forms["register"]["passV"].value;
-	fname = document.forms["register"]["fname"].value;
-	lname = document.forms["register"]["lname"].value;
-	email = document.forms["register"]["email"].value;
-	if(uname == null || uname == "") {
-		document.forms["register"]["user"].style.backgroundColor = "#ffad99";
+const validateForm = () => {
+	const errColor = "#ffad99";
+
+
+	const {user, pass, passV, fname, lname, email} = document.forms.register;
+
+	if(!user.value) {
+		user.style.backgroundColor = errColor;
 		alert("You must enter a valid username.");
 		return false;
 	}
-	if(pass == null || vpass == null || pass == "" || vpass == "") {
-		document.forms["register"]["pass"].style.backgroundColor = "#ffad99";
-		document.forms["register"]["passV"].style.backgroundColor = "#ffad99";
+	if(!pass.value || !passV.value) {
+		pass.style.backgroundColor = errColor;
+		passV.style.backgroundColor = errColor;
 		alert("You must enter a valid password.");
 		return false;
 	}
-	if(pass != vpass) {
-		document.forms["register"]["pass"].style.backgroundColor = "#ffad99";
-		document.forms["register"]["passV"].style.backgroundColor = "#ffad99";
+	if(pass.value !== passV.value) {
+		pass.style.backgroundColor = errColor;
+		passV.style.backgroundColor = errColor;
 		alert("Passwords do not match.");
 		return false;
 	}
-	if(fname == null || fname == "") {
-		document.forms["register"]["fname"].style.backgroundColor = "#ffad99";
+	if(!fname.value) {
+		fname.style.backgroundColor = errColor;
 		alert("You must enter a valid first name.");
 		return false;
 	}
-	if(lname == null || lname == "") {
-		document.forms["register"]["lname"].style.backgroundColor = "#ffad99";
+	if(!lname.value) {
+		lname.style.backgroundColor = errColor;
 		alert("You must enter a valid last name.");
 		return false;
 	}
-	if(email == null || email == "" || !email.includes("@")) {
-		document.forms["register"]["email"].style.backgroundColor = "#ffad99";
+	const emailRegex = /\S+@\S+\.\S+/;
+	if(!emailRegex.test(email.value)) {
+		email.style.backgroundColor = errColor;
 		alert("You must enter a valid email.");
 		return false;
 	}
@@ -115,14 +109,14 @@ validateForm = function() {
 /*
 * Function to reset the search form.
 */
-resetForm = function() {
+const resetForm = function() {
 	$('input[type=checkbox]').each(function() { this.checked = false; });
 }
 
 /*
 * Function to add a new chapter to a course.
 */
-addChapter = function() {
+const addChapter = () => {
 	var appendText = "";
 	var name = prompt("What is the name of this chapter?");
 	appendText = "\n~CHAPTER:name-"+name+":~\n";
@@ -132,7 +126,7 @@ addChapter = function() {
 /*
 * Function to add a new lesson to a course.
 */
-addLesson = function(lessons) {
+const addLesson = function(lessons) {
 	var appendText = "";
 	var promptText = "Enter the number of the lesson you would like to add:\n";
 	for(i = 0; i < lessons.length; i++) {
@@ -150,7 +144,7 @@ addLesson = function(lessons) {
 /*
 * Function to save a course a user has created.
 */
-saveCourse = function(id) {
+const saveCourse = function(id) {
 	if(id === null || id === '') {
 		document.getElementById('opp').value = "New";
 	} else {
@@ -165,7 +159,7 @@ saveCourse = function(id) {
 /*
 * Function to delete a course a user has created.
 */
-deleteCourse = function() {
+const deleteCourse = function() {
 	document.getElementById('opp').value = "Delete";
 	answer = confirm("Are you sure?");
 	if(answer) {
@@ -176,7 +170,7 @@ deleteCourse = function() {
 /*
 * Function to add a new quiz to a lesson.
 */
-uploadQuiz = function() {
+const uploadQuiz = function() {
 	var appendText = "";
 	var name = prompt("What do you want the question to be?");
 	var response = prompt("How many possible answers do you want there to be?");
@@ -202,7 +196,7 @@ uploadQuiz = function() {
 /*
 * Function to add a new link to a lesson.
 */
-uploadLink = function() {
+const uploadLink = function() {
 	var appendText = "";
 	var name = prompt("What should this link say?");
 	var url = prompt("Where do you want this link to go?");
@@ -213,7 +207,7 @@ uploadLink = function() {
 /*
 * Function to add a new image to a lesson.
 */
-uploadImage = function() {
+const uploadImage = function() {
 	var appendText = "";
 	var url = prompt("What is the image url?");
 	appendText = "\n~IMAGE:url-"+url+":~\n";
@@ -223,7 +217,7 @@ uploadImage = function() {
 /*
 * Function to save a lesson a user has created.
 */
-saveLesson = function(id) {
+const saveLesson = function(id) {
 	if(id === null || id === '') {
 		document.getElementById('opp').value = "New";
 	} else {
@@ -238,7 +232,7 @@ saveLesson = function(id) {
 /*
 * Function to delete a lesson a user has created.
 */
-deleteLesson = function() {
+const deleteLesson = function() {
 	document.getElementById('opp').value = "Delete";
 	answer = confirm("Are you sure?");
 	if(answer) {
