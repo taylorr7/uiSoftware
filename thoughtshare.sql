01c9c56d80e6fde43becd9c7301e36bb21c07889
Steven-Roberts <steven94@vt.edu>
2016-11-05 19:28:05 -0400

    Update database structure

--- thoughtshare.sql
+++ thoughtshare.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 03, 2016 at 07:24 PM
+-- Generation Time: Nov 06, 2016 at 12:27 AM
 -- Server version: 10.1.16-MariaDB
 -- PHP Version: 5.6.24
 
@@ -57,7 +57,8 @@ INSERT INTO `courses` (`id`, `userid`, `coursename`, `coursedescription`, `cours
 (1, 2, 'Calculus 1', 'This course is an introduction to calculus. The course begins with a review of functions and basic arithmetic then transitions into an introduction to limits. Derivatives are then introduced and their applications are studied. The course ends with a brief introduction to anti-derivatives and definite integrals.', '~CHAPTER:name-Derivatives:~\r\n\r\n~LESSON:name-Power Rule:~', 1),
 (2, 3, 'Calculus 2', 'This course is an introduction to integral calculus. The course begins with a review of antiderivatives then transitions into an introduction to definite integrals. Indefinite integrals are then introduced and their applications are studied. The course ends with a brief introduction to summations and infinite series.', '~CHAPTER:name-Chapter 1:~\r\n~LESSON:name-Product Rule:~\r\n~LESSON:name-Chain Rule:~', 1),
 (3, 3, 'My Other Calculus Course', 'This is just a copy of the same course.', '~CHAPTER:name-Chapter 1:~\r\n~LESSON:name-Product Rule:~\r\n~LESSON:name-Chain Rule:~', 1),
-(4, 4, 'Course I Am Working On', 'This is a course I am still working on so it shouldn''t be published!', '\r\n~LESSON:name-Quotient Rule:~', 0);
+(4, 4, 'Course I Am Working On', 'This is a course I am still working on so it shouldn''t be published!', '\r\n~LESSON:name-Quotient Rule:~', 0),
+(5, 2, 'Test1', 'This be a test', 'Hi', 0);
 
 -- --------------------------------------------------------
 
@@ -70,12 +71,18 @@ CREATE TABLE `event` (
   `eventtypeid` int(11) NOT NULL,
   `user1id` int(11) NOT NULL,
   `user2id` int(11) DEFAULT NULL,
-  `course1id` int(11) DEFAULT NULL,
-  `course2id` int(11) DEFAULT NULL,
-  `data1` text,
-  `data2` text,
+  `data` int(11) DEFAULT NULL,
   `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `event`
+--
+
+INSERT INTO `event` (`id`, `eventtypeid`, `user1id`, `user2id`, `data`, `timestamp`) VALUES
+(1, 2, 2, NULL, 5, '2016-11-05 18:45:59'),
+(4, 3, 2, NULL, 8, '2016-11-05 23:12:46'),
+(5, 4, 2, NULL, 8, '2016-11-05 23:18:16');
 
 -- --------------------------------------------------------
 
@@ -97,7 +104,7 @@ INSERT INTO `event_types` (`id`, `name`) VALUES
 (2, 'edit_course'),
 (3, 'new_lesson'),
 (4, 'edit_lesson'),
-(5, 'lesson_comment'),
+(5, 'course_comment'),
 (6, 'subscribe');
 
 -- --------------------------------------------------------
@@ -122,7 +129,8 @@ INSERT INTO `lessons` (`id`, `userid`, `lessonname`, `content`) VALUES
 (4, 2, 'New Lesson', 'Let us make a new lesson.'),
 (5, 3, 'Product Rule', 'If you define a function f as the product of two functions, g and h, you can define the derivative of f or f'' as (g'')h + g(h'').\r\n\r\n\r\n~QUIZ:name-What is the derivative of xcos(x)?:answer-cos(x):correctAnswer-cos(x) - xsin(x):answer-4:~\r\n'),
 (6, 3, 'Chain Rule', 'To find the derivative of a function f, defined as the composition of two functions, g and h, we define f'' as g''(h)*h''.\r\n\r\n\r\n~QUIZ:name-What is the derivative of cos(x^2)?:answer-14:correctAnswer--2xsin(x^2):~\r\n\r\nThis is the chain rule of derivatives :)'),
-(7, 4, 'Quotient Rule', 'For a function f, defined as the quotient of two functions g and h, we define the derivative of f, or f'' as [g''(h) - g(h'')] / [h^2].');
+(7, 4, 'Quotient Rule', 'For a function f, defined as the quotient of two functions g and h, we define the derivative of f, or f'' as [g''(h) - g(h'')] / [h^2].'),
+(8, 2, 'Algebra', '1+1=2\r\n\r\n~QUIZ:name-What is 1+2?:answer-2:correctAnswer-3:answer-4:~\r\n');
 
 -- --------------------------------------------------------
 
@@ -222,12 +230,12 @@ ALTER TABLE `comments`
 -- AUTO_INCREMENT for table `courses`
 --
 ALTER TABLE `courses`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 --
 -- AUTO_INCREMENT for table `event`
 --
 ALTER TABLE `event`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 --
 -- AUTO_INCREMENT for table `event_types`
 --
@@ -237,7 +245,7 @@ ALTER TABLE `event_types`
 -- AUTO_INCREMENT for table `lessons`
 --
 ALTER TABLE `lessons`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 --
 -- AUTO_INCREMENT for table `subscriptions`
 --
