1f4df0520493d769d9f1440b44049d90c88f8997
Steven-Roberts <steven94@vt.edu>
2016-11-07 12:53:32 -0500

    Update subscribe.js to ES6

--- public/scripts/subscribe.js
+++ public/scripts/subscribe.js
@@ -1,49 +1,46 @@
 /*
-* Steven Roberts
-* Hannah Roth
-* Taylor Rydahl
-*/
+ * Steven Roberts
+ * Hannah Roth
+ * Taylor Rydahl
+ */
 
-$(document).ready(function(){
-  const fullUrl = window.location.href;
-  const endIndex = fullUrl.search("authors");
-  const url = fullUrl.substr(0, endIndex+7);
-  
-  /*
-  * Checks the current subscribed status of
-  * the author when the page loads.
-  */
-	$('.subscribe').each(function(){	
-		const name = $(this).attr('name');
-		sendPost(name, "true", url);
-	});
+$(document).ready(() => {
+    /*
+     * Sends a POST Ajax request to check if
+     * the author has been subscribed to and, if
+     * a button was clicked, to update the
+     * appropriate information in the database.
+     */
+    const sendPost = (name, check) => {
+        $.post(
+            `${BASE_URL}/authors/subscribe`,
+            {name, check},
+            (data) => {
+                if (data.status === 'subscribed') {
+                    $(`[name="${name}"]`).html("<span class='glyphicon glyphicon-remove'></span> Unsubscribe");
+                } else if (data.status === 'unsubscribed') {
+                    $(`[name="${name}"]`).html("<span class='glyphicon glyphicon-ok'></span> Subscribe");
+                }
+            },
+            "json"
+        );
+    }
 
-  /*
-  * Adds the appropriate event to the
-  * subscribe button on the page.
-  */
-  $('.subscribe').click(function(){
-	const name = $(this).attr('name');
-	sendPost(name, "false", url);
-  });
+    /*
+     * Checks the current subscribed status of
+     * the author when the page loads.
+     */
+    $('.subscribe').each(function() {
+        const name = $(this).attr('name');
+        sendPost(name, "true");
+    });
+
+    /*
+     * Adds the appropriate event to the
+     * subscribe button on the page.
+     */
+    $('.subscribe').click(function() {
+        const name = $(this).attr('name');
+        sendPost(name, "false");
+    });
 });
-
-/*
-* Sends a POST Ajax request to check if
-* the author has been subscribed to and, if
-* a button was clicked, to update the 
-* appropriate information in the database.
-*/
-const sendPost = function(name, check, url) {
-	$.post(
-		url + '/subscribe/', 
-		{ 'name' : name, 'check' : check }, 
-		function(data) {
-			if(data.status == 'subscribed') { 
-				$('[name = "'+name+'"]').html("<span class='glyphicon glyphicon-edit'></span> Unsubscribe"); 
-			} else if(data.status == 'unsubscribed') { 
-				$('[name = "'+name+'"]').html("<span class='glyphicon glyphicon-edit'></span> Subscribe"); 
-			}}, 
-		"json"
-	); 
-}
