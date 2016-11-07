3fe2259738c7678dc745db5714d2b84777e39334
Steven-Roberts <steven94@vt.edu>
2016-11-07 13:07:48 -0500

    Clean up publish js

--- public/scripts/publish.js
+++ public/scripts/publish.js
@@ -1,49 +1,47 @@
 /*
-* Steven Roberts
-* Hannah Roth
-* Taylor Rydahl
-*/
+ * Steven Roberts
+ * Hannah Roth
+ * Taylor Rydahl
+ */
 
-$(document).ready(function(){
-  const fullUrl = window.location.href;
-  const endIndex = fullUrl.search("courses");
-  const url = fullUrl.substr(0, endIndex+7);
-  
-  /*
-  * Checks the current published status of
-  * each course when the page loads.
-  */
-	$('.publish').each(function(){	
-		const name = $(this).attr('name');
-		sendPost(name, "true", url);
-	});
+$(document).ready(() => {
 
-  /*
-  * Adds the appropriate event to each
-  * publish button on the page.
-  */
-  $('.publish').click(function(){
-	const name = $(this).attr('name');
-	sendPost(name, "false", url);
-  });
+    /*
+     * Sends a POST Ajax request to check if
+     * the course has been published and, if
+     * a button was clicked, to update the
+     * appropriate information in the database.
+     */
+    const sendPost = (name, check, url) => {
+        $.post(
+            `${BASE_URL}/courses/publish`,
+            {name, check},
+            (data) => {
+                if (data.status === 'published') {
+                    $(`[name="${name}"]`).html("<span class='glyphicon glyphicon-eye-close'></span> Unpublish");
+                } else if (data.status === 'unpublished') {
+                    $(`[name="${name}"]`).html("<span class='glyphicon glyphicon-eye-open'></span> Publish");
+                }
+            },
+            "json"
+        );
+    };
+
+    /*
+     * Checks the current published status of
+     * each course when the page loads.
+     */
+    $('.publish').each(function() {
+        const name = $(this).attr('name');
+        sendPost(name, true);
+    });
+
+    /*
+     * Adds the appropriate event to each
+     * publish button on the page.
+     */
+    $('.publish').click(function() {
+        const name = $(this).attr('name');
+        sendPost(name, false);
+    });
 });
-
-/*
-* Sends a POST Ajax request to check if
-* the course has been published and, if
-* a button was clicked, to update the 
-* appropriate information in the database.
-*/
-const sendPost = function(name, check, url) {
-	$.post(
-		url + '/publish/', 
-		{ 'name' : name, 'check' : check }, 
-		function(data) { 
-			if(data.status == 'published') { 
-				$('[name = "'+name+'"]').html("<span class='glyphicon glyphicon-edit'></span> Unpublish"); 
-			} else if(data.status == 'unpublished') { 
-				$('[name = "'+name+'"]').html("<span class='glyphicon glyphicon-edit'></span> Publish"); 
-			}}, 
-		"json"
-	); 
-}
