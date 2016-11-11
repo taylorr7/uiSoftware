72f6ba874b3039806e8a245809dd49c97031e790
Steven-Roberts <steven94@vt.edu>
2016-11-11 12:10:54 -0500

    Fix course delete

--- app/model/Db.class.php
+++ app/model/Db.class.php
@@ -14,7 +14,7 @@ class Db
         $username = DB_USER;
         $password = DB_PASS;
         // Create the connection with the global database config
-        $conn = mysql_connect($host, $username, $password)
+        $this->conn = mysql_connect($host, $username, $password)
         or die ('Error: Could not connect to MySql database');
 
         mysql_select_db($database);
@@ -30,7 +30,7 @@ class Db
     }
 
     // Converts a string to MySQL escaped text
-    private static function escape($text) {
+    private function escape($text) {
         if (is_null($text)) {
             return 'NULL';
         } else {
@@ -40,9 +40,9 @@ class Db
     }
 
     // Creates a MySQL clause equating the keys and values of properties
-    private static function generateQueryClause($properties, $delimiter) {
+    private function generateQueryClause($properties, $delimiter) {
         $whereClauses = array_map(function($item) use ($properties) {
-            $val = self::escape($properties[$item]);
+            $val = $this->escape($properties[$item]);
             return "{$item} = {$val}";
         }, array_keys($properties));
 
@@ -50,16 +50,17 @@ class Db
     }
 
     // Performs a database query
-    private static function query($query) {
-        $result = mysql_query($query);
+    private function query($query) {
+        echo $query;
+        $result = mysql_query($query, $this->conn);
         if (!$result)
-            die('Invalid query: ' . $query);
+            die('Invalid query: ' . mysql_error());
         return $result;
     }
 
     // Loads objects from database
-    public static function select($query, $class_name = null) {
-        $result = self::query($query);
+    public function select($query, $class_name = null) {
+        $result = $this->query($query);
         $items = array();
         if (is_null($class_name)) {
             while ($item = mysql_fetch_array($result)) {
@@ -75,12 +76,12 @@ class Db
     }
 
     // Loads all objects in a table
-    public static function selectAll($db_table, $class_name = null) {
-        return self::select("SELECT * FROM {$db_table};", $class_name);
+    public function selectAll($db_table, $class_name = null) {
+        return $this->select("SELECT * FROM {$db_table};", $class_name);
     }
 
     // Searches a table by properties
-    public static function search($db_table, $properties, $qry, $class_name = null) {
+    public function search($db_table, $properties, $qry, $class_name = null) {
         $whereClause = implode(array_map(function($item) use ($qry) {
             return "INSTR({$item}, '{$qry}') > 0";
         }, $properties), " OR ");
@@ -90,59 +91,58 @@ class Db
         }, $properties), ", ");
 
         $query = "SELECT * FROM {$db_table} WHERE {$whereClause} ORDER BY {$orderByClause};";
-        return self::select($query, $class_name);
+        return $this->select($query, $class_name);
     }
 
     // Loads objects when given properties match
-    public static function selectByProperties($db_table, $properties, $class_name = null) {
-        $whereClause = self::generateQueryClause($properties, " AND ");
+    public function selectByProperties($db_table, $properties, $class_name = null) {
+        $whereClause = $this->generateQueryClause($properties, " AND ");
         $query = "SELECT * FROM {$db_table} WHERE {$whereClause};";
-        return self::select($query, $class_name);
+        return $this->select($query, $class_name);
     }
 
     // Loads object when single property matches
-    public static function selectByProperty($db_table, $column, $val, $class_name = null) {
-        return self::selectByProperties($db_table, array($column => $val), $class_name);
+    public function selectByProperty($db_table, $column, $val, $class_name = null) {
+        return $this->selectByProperties($db_table, array($column => $val), $class_name);
     }
 
     // Loads object from database with given id
-    public static function selectById($db_table, $id, $class_name = null) {
-        return self::selectByProperty($db_table, "id", $id, $class_name);
+    public function selectById($db_table, $id, $class_name = null) {
+        return $this->selectByProperty($db_table, "id", $id, $class_name);
     }
 
     // Performs update of existing data
-    public static function update($db_table, $id, $properties) {
-        $setClause = self::generateQueryClause($properties, ", ");
+    public function update($db_table, $id, $properties) {
+        $setClause = $this->generateQueryClause($properties, ", ");
         $query = "UPDATE {$db_table} SET {$setClause} WHERE id='{$id}';";
-        return self::query($query);
+        return $this->query($query);
     }
 
     // Adds new row to table
-    public static function insert($db_table, $properties) {
+    public function insert($db_table, $properties) {
         $columns = implode(array_keys($properties), ', ');
         $values = implode(array_map(function($val) {
-            return self::escape($val);
+            return $this->escape($val);
         }, array_values($properties)), ', ');
         $query = "INSERT INTO {$db_table} ({$columns}) VALUES ({$values});";
-        self::query($query);
+        $this->query($query);
         return mysql_insert_id();
     }
 
     // Deletes rows that match properties
-    public static function deleteByProperties($db_table, $properties) {
-        $whereClause = self::generateQueryClause($properties, " AND ");
+    public function deleteByProperties($db_table, $properties) {
+        $whereClause = $this->generateQueryClause($properties, " AND ");
         $query = "DELETE FROM {$db_table} WHERE {$whereClause};";
-        self::query($query);
+        return $this->query($query);
     }
 
     // Deletes row that matches property
-    public static function deleteByProperty($db_table, $column, $val) {
-        return self::deleteByProperties($db_table, array($column => $val));
+    public function deleteByProperty($db_table, $column, $val) {
+        return $this->deleteByProperties($db_table, array($column => $val));
     }
 
     // Deletes row that has given id
-    public static function deleteById($db_table, $id) {
-        return self::deleteByProperty($db_table, "id", $id);
-
+    public function deleteById($db_table, $id) {
+        return $this->deleteByProperty($db_table, "id", $id);
     }
 }
